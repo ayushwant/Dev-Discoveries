17bf95febd3f3b70a1d5bda8aba703f00f1e19a0
ayushwant <ayushwant912@gmail.com>
2023-09-09 10:50:34 +0530

    did component scanning

--- Spring/Spring Boot/02-Spring Boot Spring Core/01-Constructor Injection/src/main/java/com/example/springcoredemo/SpringCoreDemoApplication.java
+++ Spring/Spring Boot/02-Spring Boot Spring Core/01-Constructor Injection/src/main/java/com/example/springcoredemo/SpringCoreDemoApplication.java
@@ -3,7 +3,10 @@ package com.example.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+//		scanBasePackages = { "com.example.springcoredemo",
+//		                      "com.example.util" }
+)
 public class SpringCoreDemoApplication {
 
 	public static void main(String[] args) {
