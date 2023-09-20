0328b310250fc67442f5260e6832fabc7f7e5f77
ayushwant <ayushwant912@gmail.com>
2023-09-20 16:04:13 +0530

    added ID path variable

--- Spring/Spring Boot/04- Spring Boot REST CRUD/01- Spring Boot Controller Demo/src/main/java/com/example/demo/rest/StudentRestController.java
+++ Spring/Spring Boot/04- Spring Boot REST CRUD/01- Spring Boot Controller Demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.example.demo.rest;
 
 import com.example.demo.model.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +15,30 @@ import java.util.List;
 @RequestMapping("api")
 public class StudentRestController
 {
-    @GetMapping("students")
-    public ArrayList<Student> getStudents()
+    List<Student> students;
+
+    // use @PostConstruct to load the data only once.
+    @PostConstruct
+    public void loadData()
     {
-        ArrayList<Student> students = new ArrayList<>();
+        students = new ArrayList<>();
 
         students.add(new Student("Poornima", "Patel"));
         students.add(new Student("John", "Kennedy"));
         students.add(new Student("George", "Bush"));
+    }
 
+    @GetMapping("students")
+    // the name of method doesn't matter. Only the URL endpoint matters
+    public List<Student> getStudents()
+    {
         return students;
+    }
+
+    @GetMapping("students/{ID}")
+    // the name of method doesn't matter. Only the URL endpoint matters
+    public Student getStudent(@PathVariable int ID){
+        return students.get(ID);
     }
 
 }
