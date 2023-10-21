ae8d2305beb640594df96fa21345001c0b7b9368
ayushwant <ayushwant912@gmail.com>
2023-10-21 11:16:48 +0530

    working graphql, just connected to postgres

---
+++ Spring/pgraphql/src/main/java/com/ayushwant/pgraphql/PgraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.ayushwant.pgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgraphqlApplication.class, args);
+	}
+
+}
---
+++ Spring/pgraphql/src/main/java/com/ayushwant/pgraphql/model/Employee.java
@@ -0,0 +1,59 @@
+package com.ayushwant.pgraphql.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee
+{
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    String id;
+    String first_name;
+    String last_name;
+    String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id='" + id + '\'' +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
