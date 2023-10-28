374463a2a96da587cb0c55eb34536b3bed51bf94
ayushwant <ayushwant912@gmail.com>
2023-10-28 09:41:19 +0530

    Learnt Qualifiers

--- Spring/Spring Boot/02-Spring Boot Spring Core/01-Constructor Injection/src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ Spring/Spring Boot/02-Spring Boot Spring Core/01-Constructor Injection/src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.springcoredemo.rest;
 
 import com.example.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController
     private Coach myCoach;
 
     @Autowired
-    public DemoController(Coach theCoach){
+    public DemoController( @Qualifier("baseballCoach") Coach theCoach){
         this.myCoach = theCoach;
     }
 
